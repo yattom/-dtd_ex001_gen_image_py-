24fa9b882add8fe53ebd6077d263a9b2703bf89d
yattom <tsutomu.yasui@gmail.com>
2024-03-02 09:39:51 +0900

    blend two images to generate gradation

--- gen.py
+++ gen.py
@@ -5,22 +5,31 @@ from PIL import Image
 width = 800
 height = 600
 
-# Create a NumPy array representing the image data
-# In this example, we'll create a gradient image
-# You can generate any image data you desire here
-image_data = np.zeros((height, width, 3), dtype=np.uint8)
+# Create arrays for black-to-red and blue-to-black gradients
+black_to_red = np.zeros((height, width, 4), dtype=np.uint8)
+blue_to_black = np.zeros((height, width, 4), dtype=np.uint8)
 
-# Generate a gradient from black to white horizontally
+# Generate black-to-red gradient
 for y in range(height):
     for x in range(width):
-        r = int((x / width) * 255)  # Red component
-        b = int(((width - x) / width) * 255)  # Blue component
+        black_to_red[y, x] = [255, 0, 0, int(x / width * 255)]
 
-        # Set the pixel color
-        image_data[y, x] = [r, 0, b]  # Red, Green, Blue
+# Generate blue-to-black gradient
+for y in range(height):
+    for x in range(width):
+        blue_to_black[y, x] = [0, 0, 255, int((width - x) / width * 255)]
+
+# Create a blended gradient using alpha blending
+alpha_channel_normalized = blue_to_black[:, :, 3] / 255.0  # Normalize alpha channel
+blended_gradient = black_to_red.copy()  # Initialize with black-to-red gradient
+
+# Apply alpha blending
+
+blended_gradient[:, :, :3] = (blended_gradient[:, :, :3] * (1.0 - alpha_channel_normalized[:, :, None]) +
+                              blue_to_black[:, :, :3] * alpha_channel_normalized[:, :, None]).astype(np.uint8)
 
 # Create a Pillow Image object from the NumPy array
-image = Image.fromarray(image_data)
+image = Image.fromarray(blended_gradient[:, :, :3], 'RGB')
 
 # Save the image to a file
 image.save("gradient_image.png")
